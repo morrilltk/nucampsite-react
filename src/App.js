a73c89a400989af145032385c2b64305b87fbab2
Tregan Morrill <Morrilltk@gmail.com>
2022-12-09 16:45:53 -0800

    completed list, detail, and comment sections

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import { Routes, Route } from 'react-router-dom';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
 import './App.css';
-import CampsitesList from './features/campsites/CampsitesList';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
@@ -7,7 +11,12 @@ function App() {
   return (
     <div className="App">
       <Header />
-        <CampsitesList />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='contact' element={<ContactPage />} />
+          <Route path='directory' element={<CampsitesDirectoryPage />} />
+          <Route path='directory/:campsiteId' element={<CampsiteDetailPage />} />
+        </Routes>
       <Footer />
     </div>
   );
--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -1,15 +1,20 @@
-import { CAMPSITES } from '../../app/shared/CAMPSITES'
 import {Row, Col} from 'reactstrap'
 import CampsiteCard from './CampsiteCard'
+import { selectAllCampsites } from './campsitesSlice'
 
-const CampsitesList =() => {
+const CampsitesList = () => {
+    const campsites = selectAllCampsites()
     return (
         <Row className='ms-auto'>
             {
-                CAMPSITES.map(campsite => {
+                campsites.map(campsite => {
                     return (
-                        <Col md='5' className='m-4' key={campsite.id}>
-                         <CampsiteCard campsite={campsite}/>
+                        <Col 
+                            md='5' 
+                            className='m-4' 
+                            key={campsite.id}
+                        >
+                            <CampsiteCard campsite={campsite}/>
                         </Col>
                     )
                 })
