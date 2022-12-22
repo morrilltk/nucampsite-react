ab9733d0796885e0b0b924afc7d2341eb809342e
Tregan Morrill <Morrilltk@gmail.com>
2022-12-21 17:53:35 -0800

    added comment

--- src/features/comments/CommentForm.js
+++ src/features/comments/CommentForm.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addComment } from "./commentsSlice";
 import {
   Button,
   Modal,
@@ -12,14 +14,16 @@ import { validateCommentForm } from "../../utils/validateCommentForm";
 
 const CommentForm = ({ campsiteId }) => {
   const [modalOpen, setModalOpen] = useState(false);
+  const dispatch = useDispatch();
   const handleSubmit = values => {
     const comment = {
       campsiteId: parseInt(campsiteId),
       rating: values.rating,
       author: values.author,
       text: values.commentText,
+      date: new Date(Date.now()).toISOString(),
     };
-    console.log(comment);
+    dispatch(addComment(comment));
     setModalOpen(false);
   };
   return (
