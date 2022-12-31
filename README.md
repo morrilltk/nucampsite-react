d8e5eb4b61d3e1d97659d0b63cca9cf6a45802a6
Tregan Morrill <Morrilltk@gmail.com>
2022-12-30 16:33:48 -0800

    sync Campsites

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+this project has to be run with json server
+`json-server -H 0.0.0.0 --watch db.json -p 3001 -d 2000`
+
 # Getting Started with Create React App and Redux
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app), using the [Redux](https://redux.js.org/) and [Redux Toolkit](https://redux-toolkit.js.org/) template.
--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
-import { Routes, Route } from 'react-router-dom';
-import ContactPage from './pages/ContactPage';
-import HomePage from './pages/HomePage';
-import './App.css';
-import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
-import CampsiteDetailPage from './pages/CampsiteDetailPage';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import AboutPage from './pages/AboutPage';
+import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import ContactPage from "./pages/ContactPage";
+import HomePage from "./pages/HomePage";
+import "./App.css";
+import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
+import CampsiteDetailPage from "./pages/CampsiteDetailPage";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import AboutPage from "./pages/AboutPage";
+import { fetchCampsites } from "./features/campsites/campsitesSlice";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCampsites());
+  }, [dispatch]);
+
   return (
-    <div className="App">
+    <div className='App'>
       <Header />
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='contact' element={<ContactPage />} />
-          <Route path='directory' element={<CampsitesDirectoryPage />} />
-          <Route path='directory/:campsiteId' element={<CampsiteDetailPage />} />
-          <Route path='about' element={<AboutPage />} />
-        </Routes>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='contact' element={<ContactPage />} />
+        <Route path='directory' element={<CampsitesDirectoryPage />} />
+        <Route path='directory/:campsiteId' element={<CampsiteDetailPage />} />
+        <Route path='about' element={<AboutPage />} />
+      </Routes>
       <Footer />
     </div>
   );
 }
 
-export default App; 
+export default App;
